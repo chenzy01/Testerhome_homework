ea972ddec7e84f66b9c443130ba2245d0f28c8c7
chenzy01 <chenzy.hyx@gmail.com>
2020-05-22 00:37:08 +0800

    增加 jsonpath 解析

--- test_wework_api/api/BaseApi.py
+++ test_wework_api/api/BaseApi.py
@@ -3,5 +3,13 @@ import pprint
 
 
 class BaseApi:
-    def verbose(self, json_object):
+    json_data = None
+
+    @classmethod
+    def verbose(cls, json_object=json_data):
         print(Utils.json_format(json_object))
+
+    @classmethod
+    def json_path(cls, expr):
+        return Utils.json_path(cls.json_data, expr)
+
