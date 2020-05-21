4100f79474fb217c11e10b92ac7481e82dd4cc18
chenzy01 <chenzy.hyx@gmail.com>
2020-05-22 00:39:02 +0800

    增加 json_path 方法，对文档进行解析

--- test_wework_api/api/BaseApi.py
+++ test_wework_api/api/BaseApi.py
@@ -7,9 +7,11 @@ class BaseApi:
 
     @classmethod
     def verbose(cls, json_object=json_data):
+        # 将数据转换为 json 格式
         print(Utils.json_format(json_object))
 
     @classmethod
     def json_path(cls, expr):
+        # 使用 jsonpath 对内容进行定位，expr 是要定位的内容
         return Utils.json_path(cls.json_data, expr)
 
