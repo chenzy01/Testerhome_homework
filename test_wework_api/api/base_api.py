f5058970bf8a0f9e9f7a56b6c71f01f0293d261c
chenzy01 <chenzy.hyx@gmail.com>
2020-05-23 17:47:02 +0800

    增加获取 access_token 的方法

--- test_wework_api/api/base_api.py
+++ test_wework_api/api/base_api.py
@@ -1,3 +1,6 @@
+import requests
+
+
 from test_wework_api.utils.Utils import Utils
 import pprint
 
@@ -15,4 +18,13 @@ class BaseApi:
     def json_path(cls, expr):
         # 使用 jsonpath 对内容进行定位，expr 是要定位的内容
         return Utils.json_path(cls.json_data, expr)
+
+    def request(self, method, url, params: dict, json, data):
+        from test_wework_api.api.wework import WeWork
+        self.json_data = requests.request(method, url=url,
+                                          params=params.update({"access_token": WeWork.get_app_token()}))
+        self.verbose(self.json_data)
+        return self.json_data
+
+
 
