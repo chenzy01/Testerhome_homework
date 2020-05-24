608908c07a151cb22c52eb804ae99927d74510fc
chenzy01 <chenzy.hyx@gmail.com>
2020-05-25 00:11:29 +0800

    更新部门接口测试，用时间戳做测试数据

--- test_wework_api/testcases/it/test_department.py
+++ test_wework_api/testcases/it/test_department.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from test_wework_api.api.department import Department
 from test_wework_api.utils.Utils import Utils
 
@@ -14,14 +16,19 @@ class TestDepartment:
         assert d["department"][0]["name"] == "不止测试"
 
     def test_create(self):
-        r = self.department.create("1002", 1, 100000001, 7)
+        now = datetime.now()
+        name = "子部门_" + str(now.hour) + str(now.second)
+        r = self.department.create(name, 1, 100000001)
         assert r["errcode"] == 0
         assert r["id"] is not None
-        exist = False
-        for depart in self.department.list("")["department"]:
-            if depart["id"] == r["id"]:
-                exist = True
-        assert exist == True
+        self.department.list()
+        assert self.department.json_path("$.department[?(@.id==%s)]" % r["id"])[0]["name"] == name
+
+        # exist = False
+        # for depart in self.department.list("")["department"]:
+        #     if depart["id"] == r["id"]:
+        #         exist = True
+        # assert exist == True
 
     def test_delete(self):
         r = self.department.delete(6)
